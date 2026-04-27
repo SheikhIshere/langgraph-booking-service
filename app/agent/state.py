da09fadd083bab78cb4b8cee1fb92ea659ef22d5
SheikhIshere <thesheikh255@gmail.com>
2026-04-27 15:27:39 +0600

    feat: setup langgraph state and core tools

---
+++ app/agent/state.py
@@ -0,0 +1,7 @@
+from typing import TypedDict, Annotated, List
+from langchain_core.messages import BaseMessage
+import operator
+
+class AgentState(TypedDict):
+    messages: Annotated[List[BaseMessage], operator.add]
+    escalation_status: str # "none", "offered", or "escalated"
