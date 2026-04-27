01858177f6f69a8b0db435bb7cf2b4bc5c25e97e
SheikhIshere <thesheikh255@gmail.com>
2026-04-27 16:51:10 +0600

    fix: making deploy ready

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
     GROQ_API_KEY: str
+    ALLOW_ORIGINS: list[str] = ["*"]
 
     class Config:
         env_file = ".env"
